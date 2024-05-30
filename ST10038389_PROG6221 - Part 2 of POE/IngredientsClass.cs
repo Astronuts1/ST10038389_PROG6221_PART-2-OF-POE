30dd93de1dd273e36f34973a3c06246deb955c32
Luqmaan Aziz - ST10038389 <129889786+Astronuts1@users.noreply.github.com>
2024-05-30 15:01:46 +0200

    Update IngredientsClass.cs
    Updated the Ingredients Class Comments

--- ST10038389_PROG6221 - Part 2 of POE/IngredientsClass.cs
+++ ST10038389_PROG6221 - Part 2 of POE/IngredientsClass.cs
@@ -4,17 +4,28 @@ namespace ST10038389_PROG6221___Part_2_of_POE
 {
     public class IngredientClass
     {
+        //<Summary>
         //Getters and Setters for Ingredient Class.
         public string Name { get; set; }
-        //Gets the String Name of Recipe and sets it into application.
+        //Gets the String Name of the Recipe and sets it into the application.
+        //This property represents the name of the ingredient as a string.
+        //It's used to identify the ingredient in the Recipe Application!
+        //Example: Milk | Sugar | Strawberries and etc.....
         public double Quantity { get; set; }
         //Getters and Setters of Quantity in Recipe Class.
+        //This property represents how much is needed!
         public string Unit { get; set; }
-        //Getters and Setters for unit of measurement in recipe class.
+        //Getters and Setters for the unit of measurement in recipe class.
+        //This property is used to measure what the quantity is in!
+        //For example: Teaspoon | Cups | Bowls | Liters(L) and Millimeters(ML).
         public int Calories { get; set; }
-        //Getters and Setters for calculating the amount of calories in recipe application.
+        //Getters and Setters are used to calculate the number of calories in the recipe application.
+        //This property represents the calories towards the quantity of the Recipe Application.
+        //For example: 50Calories | 100Calories | 150Calories | 200Calories and max is set to 300Calories!
         public string FoodGroup { get; set; }
-        //Getters and Setters for managing what each ingredient is labelled for Food Group.
+        //Getters and Setters manage which ingredient is labeled for Food Group.
+        //For example: {DIARY} | {PROTEIN} | {SWEETS} | {FAT}
+        //</Summary>
     }
 }
 //----------------------- END OF LINE -------------------------------------------------------------------------------------------------------------------------------- 

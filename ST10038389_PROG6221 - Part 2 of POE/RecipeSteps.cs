842f998aea4fa268771d7fb3cd79cc58d8d4b9c8
Luqmaan Aziz - ST10038389 <129889786+Astronuts1@users.noreply.github.com>
2024-05-30 15:09:46 +0200

    Update RecipeSteps.cs
    Added more detail to RecipeSteps Class

--- ST10038389_PROG6221 - Part 2 of POE/RecipeSteps.cs
+++ ST10038389_PROG6221 - Part 2 of POE/RecipeSteps.cs
@@ -5,7 +5,10 @@ namespace ST10038389_PROG6221___Part_2_of_POE
     internal class RecipeSteps
     {
         public string Description { get; set; }
-        // Allows the getting and setting a string value and providing a simple effective way to manage date within this class.
+        // Allows the getting and setting of a string value and provides a simple effective way to manage data within this class.
+        //This property displays the data in the form of a display!
+        //The inputted data from the user is stored in a string  when the user clicks on option 2 in the menu option.
+        //The user can see all the information details of the recipes he has added and scaled in the application!
     }
 }
 //----------------------------------------- END OF FILE --------------------------------------------------------------------------------------------------------------
